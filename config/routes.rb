823063be69defacec8a6f9e2e5d4024561e85ce0
TKbergamot <berg4m0t@gmail.com>
2020-05-06 23:07:43 +0900

    [Add] home/index,about

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,45 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :doctors, controolers:{
+
+  root 'home#index'
+
+  get 'about', to: 'home#about'
+
+  devise_for :doctors, controolers: {
+
     sessions: 'doctors/sessions',
+
     passwords: 'doctors/passwords',
+
     registrations: 'doctors/registrations'
+
   }
+
+
 
   devise_for :patients, controllers: {
+
     sessions: 'patients/sessions',
+
     passwords: 'patients/passwords',
+
     registrations: 'patients/registrations'
+
   }
 
+
+
   resource :patient
+
   resources :doctors
-  resources :posts
-  resources :opinions
-  resources :patient_comments
-  resources :doctor_comments
+
+  resources :posts, :opinions, :patient_comments, :doctor_comments
+
   resources :hospitals
+
   resources :departments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
